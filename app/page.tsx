6a86a4e6ea17dbd5a8e7c64716196605a8005815
TejasAnand <anandtejas455@gmail.com>
2024-04-18 13:46:48 -0400

    db fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,19 @@
-// "use client";
+"use client";
+
 import Image from "next/image";
-import { createClient } from "../utlis/supabase/server";
-import { cookies } from "next/headers";
 // import InitialButton from "@/components/initialButton";
 import Thread from "@/components/thread";
 // import Spline from "@splinetool/react-spline";
 
-// export default async function Page() {
-//   const cookieStore = cookies();
-//   const supabase = createClient(cookieStore);
-
-//   const { data: todos } = await supabase.from("todos").select();
-
-//   return (
-//     <ul>
-//       {todos?.map((todo) => (
-//         <li>{todo}</li>
-//       ))}
-//     </ul>
-//   );
-// }
-
-export default async function Page() {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
-
-  const { data: todos } = await supabase.from("todos").select();
-
+export default function Home() {
   return (
     <main className="main">
       <div className="messageThread">
         <Thread></Thread>
       </div>
-      <div className="spline">
-        {/* <Spline scene="https://prod.spline.design/3G7HY84V1ZUKGVgm/scene.splinecode" /> */}
-      </div>
+      {/* <div className="spline">
+        <Spline scene="https://prod.spline.design/3G7HY84V1ZUKGVgm/scene.splinecode" />
+      </div> */}
     </main>
   );
 }
--- supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient, SupabaseClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://kprthycifssoditavpyl.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwcnRoeWNpZnNzb2RpdGF2cHlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM0MjA1ODgsImV4cCI6MjAyODk5NjU4OH0._50QuQWXvCIwo9qVwkSbidH0v304KA9m88tt2GYSzhI";
-
-export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
