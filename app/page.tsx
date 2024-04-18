9ba0918c51cddf3df805b14dd3bb570e6a7abcec
TejasAnand <anandtejas455@gmail.com>
2024-04-18 13:37:42 -0400

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,39 @@
+// "use client";
 import Image from "next/image";
+import { createClient } from "../utlis/supabase/server";
+import { cookies } from "next/headers";
 // import InitialButton from "@/components/initialButton";
 import Thread from "@/components/thread";
+// import Spline from "@splinetool/react-spline";
 
-export default function Home() {
+// export default async function Page() {
+//   const cookieStore = cookies();
+//   const supabase = createClient(cookieStore);
+
+//   const { data: todos } = await supabase.from("todos").select();
+
+//   return (
+//     <ul>
+//       {todos?.map((todo) => (
+//         <li>{todo}</li>
+//       ))}
+//     </ul>
+//   );
+// }
+
+export default async function Page() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const { data: todos } = await supabase.from("todos").select();
+
   return (
-    <main>
-      <div className="initialButton">
-        {/* <InitialButton></InitialButton> */}
-      </div>
+    <main className="main">
       <div className="messageThread">
         <Thread></Thread>
+      </div>
+      <div className="spline">
+        {/* <Spline scene="https://prod.spline.design/3G7HY84V1ZUKGVgm/scene.splinecode" /> */}
       </div>
     </main>
   );
--- components/thread.tsx
+++ components/thread.tsx
@@ -2,48 +2,121 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import Image from "next/image";
 
-let messages: [string, string][] = [["user", "hi"]];
+type Message = { type: "text" | "image"; content: string };
+
+let messages: [string, Message[]][] = [["", []]];
 
 export default function Thread() {
   const [userType, setUserType] = useState<"user" | "doctor">("user");
   const [inputValue, setInputValue] = useState("");
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleClientClick = () => {
     setUserType(userType === "user" ? "doctor" : "user");
-    console.log("userType:", userType);
   };
 
   const handleSendMessage = () => {
-    // Send the inputValue to the server or perform any other necessary actions
+    const newMessages: Message[] = [];
 
-    messages = [...messages, [userType, inputValue]];
+    if (inputValue.trim() !== "") {
+      newMessages.push({ type: "text", content: inputValue });
+    }
 
-    console.log("Sent message:", inputValue);
+    if (imageFile !== null) {
+      newMessages.push({
+        type: "image",
+        content: URL.createObjectURL(imageFile),
+      });
+    }
 
-    setInputValue(""); // Clear the input field after sending the message
+    if (messages.length > 0 && messages[messages.length - 1][0] === userType) {
+      // If the previous message was sent by the same userType, add the message(s) to the last inner array
+      messages[messages.length - 1][1].push(...newMessages);
+    } else {
+      // Otherwise, add a new inner array with the userType and a new array with the message(s)
+      messages = [...messages, [userType, newMessages]];
+    }
+
+    setInputValue("");
+    setImageFile(null);
+    if (fileInputRef.current) {
+      fileInputRef.current.value = ""; // Clear the file input field
+    }
+    scrollToBottom();
   };
+
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setImageFile(e.target.files[0]);
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSendMessage();
+    }
+  };
+
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, []);
 
   return (
     <div className="messageUI">
-      <Button onClick={handleClientClick} variant="outline">
-        Switch to {userType}
+      <Button
+        className="userStyling"
+        onClick={handleClientClick}
+        variant="outline"
+      >
+        Switch to {userType === "user" ? "Doctor" : "User"}
       </Button>
-      <ScrollArea className="h-[700px] w-[800px] rounded-md border p-4">
-        {messages.map(([type, value], index) => (
+      <ScrollArea className="scrollThread h-[700px] w-[800px] rounded-md border p-4">
+        {messages.map(([type, values], index) => (
           <div key={index}>
-            {type}
-            <br />
-            {value}
+            {type !== "" && <span className="userTypeStyle">{type}</span>}
+            {values.map((message, valueIndex) => (
+              <div key={`${index}-${valueIndex}`}>
+                {message.type === "text" ? (
+                  message.content
+                ) : (
+                  <Image
+                    src={message.content}
+                    alt="Uploaded"
+                    className="max-w-full h-auto"
+                    width={250}
+                    height={250}
+                  />
+                )}
+              </div>
+            ))}
           </div>
         ))}
+        <div style={{ float: "left", clear: "both" }} ref={messagesEndRef} />
       </ScrollArea>
       <div className="messageLine">
         <Input
           placeholder="Type a message"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <Input
+          type="file"
+          accept="image/*"
+          onChange={handleImageUpload}
+          className="mr-2"
+          ref={fileInputRef}
         />
         <Button className="sendButton" onClick={handleSendMessage}>
           Send
